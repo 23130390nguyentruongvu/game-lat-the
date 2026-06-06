7f7fd119c4a77bd7722e81a75530e03d2402c228
linh-1279 <23130171@st.hcmuaf.edu.vn>
2026-06-06 17:14:02 +0700

    Up UC-9

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.infix.gamelatthe"
+    compileSdk {
+        version = release(36)
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.infix.gamelatthe"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildFeatures { viewBinding = true }
+}
+
+dependencies {
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.fragment.testing)
+    androidTestImplementation(libs.core.testing)
+
+    implementation(libs.glide)
+    implementation(libs.room.runtime)
+    implementation(libs.fragment)
+    implementation(libs.cardview)
+    implementation(libs.recyclerview)
+    implementation(libs.navigation.fragment)
+    testImplementation(libs.room.testing)
+    annotationProcessor(libs.room.compiler)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.core.testing)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation("androidx.fragment:fragment:1.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.6.2")
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
+}
